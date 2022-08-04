3b09f5eb88ebc92fc2985b188f95cf3bb1c8c9e6
Akhadafi <khadafi.dev@gmail.com>
2022-08-04 21:05:30 +0700

    v 0.0.1 (Many to Many Relationship and Query)

--- books/views.py
+++ books/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Book, Review
 from django.views.generic import ListView, DetailView
@@ -15,7 +14,16 @@ class BookDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["reviews"] = context["book"].review_set.order_by("-created_at")
+        context["authors"] = context["book"].authors.all()
         return context
+
+
+def author(request, author):
+    books = Book.objects.filter(authors__name=author)
+    context = {
+        "book_list": books,
+    }
+    return render(request, "books/book_list.html", context)
 
 
 def review(request, id):
