1435326b9f3851c3d9ad3395be9631be0d05643f
Akhadafi <khadafi.dev@gmail.com>
2022-08-04 11:01:01 +0700

    v 0.0.1 (dynamic routing)

--- books/views.py
+++ books/views.py
@@ -11,3 +11,16 @@ def index(request):
         "books": data,
     }
     return render(request, "books/index.html", context)
+
+
+def show(request, id):
+    singleBook = list()
+
+    for book in data:
+        if book["id"] == id:
+            singleBook = book
+
+    context = {
+        "book": singleBook,
+    }
+    return render(request, "books/show.html", context)
